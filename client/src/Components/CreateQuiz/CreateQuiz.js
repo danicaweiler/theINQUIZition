a402e7c75c4ba3b9d6a7bd7920e1c1a3492b35a1
Maja <mvrebac6652@conestpgac.on.ca>
2021-03-25 12:52:27 -0400

    changes during liveshare

--- client/src/Components/CreateQuiz/CreateQuiz.js
+++ client/src/Components/CreateQuiz/CreateQuiz.js
@@ -1,47 +1,68 @@
 import React from "react";
-import SimpleReactValidator from 'simple-react-validator';
+import SimpleReactValidator from "simple-react-validator";
+import axios from "axios";
+import { withRouter, Route } from "react-router-dom";
 
 class CreateQuiz extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       questionList: [],
-      questions: [ { question: "", answerA: "", answerB: "", answerC: "", answerD: "" } ],
+      questions: [
+        { question: "", answerA: "", answerB: "", answerC: "", answerD: "" },
+      ],
       username: "",
-      title: ""
+      title: "",
+      sessionId: "",
+      question: "",
     };
-    
-  this.validator = new SimpleReactValidator();
-  this.onAddBtnClick = this.onAddBtnClick.bind(this);
-  this.submitForm = this.submitForm.bind(this);
-  this.QuestionInput = this.QuestionInput.bind(this);
-  this.setTitle = this.setTitle.bind(this);
-  this.setUsername = this.setUsername.bind(this);
+
+    this.validator = new SimpleReactValidator();
+    this.onAddBtnClick = this.onAddBtnClick.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this.QuestionInput = this.QuestionInput.bind(this);
+    this.setTitle = this.setTitle.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+
+    this.setQuestion = this.setQuestion.bind(this);
   }
 
   onAddBtnClick() {
-    this.validator.hideMessages()
-    
+    this.validator.hideMessages();
+
     var validQuestions = this.state.questions;
-    var newQuestionBase = { question: "", answerA: "", answerB: "", answerC: "", answerD: "" };
+    var newQuestionBase = {
+      question: "",
+      answerA: "",
+      answerB: "",
+      answerC: "",
+      answerD: "",
+    };
     validQuestions.push(newQuestionBase);
 
     var newList = this.state.questionList;
-    var newElem = this.QuestionInput(newList.length + 1)
+    var newElem = this.QuestionInput(newList.length + 1);
     newList.push(newElem);
 
     this.setState({
       questionList: newList,
-      questions: validQuestions
+      questions: validQuestions,
     });
-  };
+  }
 
   submitForm() {
     this.validator.hideMessages();
     if (this.validator.allValid()) {
-      alert('You submitted the form and stuff!');
-      //Send form data to server here
-      // redirect to quiz page
+      alert("You submitted the form and stuff!");
+      const data = {
+        title: this.state.title,
+        username: this.state.username,
+      };
+      //  axios.post("/api/v1/create-quiz").then((res) => {
+      //     this.state.sessionId(res.data);
+      //   });
+
+      this.props.history.push("/quiz");
     } else {
       this.validator.showMessages();
       this.forceUpdate();
@@ -50,72 +71,139 @@ class CreateQuiz extends React.Component {
 
   QuestionInput(props) {
     return (
-    <div key={props}>
-      <label>Question {props}:<br />
-        <input name="question" type="text" className="question-input main" placeholder="Your question here" />
-      </label><br />
-      <label><input type="radio" id="answerA" name= {props} defaultChecked/> A 
-        <input name="A" className="question-input" placeholder="Your first answer here" />
-      </label><br />
-      <label><input type="radio" id="answerB"  name= {props}/> B 
-        <input name="B" className="question-input" placeholder="Your second answer here" />
-      </label><br />
-      <label><input type="radio" id="answerC" name= {props}/> C 
-        <input name="C" className="question-input" placeholder="Your third answer here" />
-      </label><br />
-      <label><input type="radio" id="answerD"  name= {props} /> D 
-        <input name="D" className="question-input" placeholder="Your fourth answer here" />
-      </label><br />
-      <br /><br />
-    </div>
+      <div key={props}>
+        <label>
+          Question {props}:<br />
+          <input
+            name='question'
+            type='text'
+            onChange={(e) => this.setQuestion(e, props)}
+            className='question-input main'
+            placeholder='Your question here'
+          />
+        </label>
+        <br />
+        <label>
+          <input type='radio' id='answerA' name={props} defaultChecked /> A
+          <input
+            name='A'
+            className='question-input'
+            placeholder='Your first answer here'
+          />
+        </label>
+        <br />
+        <label>
+          <input type='radio' id='answerB' name={props} /> B
+          <input
+            name='B'
+            className='question-input'
+            placeholder='Your second answer here'
+          />
+        </label>
+        <br />
+        <label>
+          <input type='radio' id='answerC' name={props} /> C
+          <input
+            name='C'
+            className='question-input'
+            placeholder='Your third answer here'
+          />
+        </label>
+        <br />
+        <label>
+          <input type='radio' id='answerD' name={props} /> D
+          <input
+            name='D'
+            className='question-input'
+            placeholder='Your fourth answer here'
+          />
+        </label>
+        <br />
+        <br />
+        <br />
+      </div>
     );
   }
 
   setTitle(e) {
-    this.setState({title: e.target.value});
+    this.setState({ title: e.target.value });
   }
 
   setUsername(e) {
-    this.setState({username: e.target.value});
+    this.setState({ username: e.target.value });
   }
 
-  setAnswerC(e) {
-    console.log(e)
+  setQuestion(e, props) {
+    this.setState({ question: e.target.value + props });
   }
 
   render() {
     return (
-  <div id="createQuiz">
-    <div className='game-header alternate'>
-      <h2>The InQUIZition</h2>
-    </div>
-    <div className="game-body">
-      <form className="form-body">
-    <h2>Build Your Quiz</h2><div className="form-group">
-      <label>
-        Username: 
-        <input className="question-input" onChange={this.setUsername} value={this.state.username} type="text" name="username" />
-        {this.validator.message('username', this.state.username, 'required|min:3|max:40')}
-      </label></div>
-    <br /><div className="form-group">
-      <label>
-        Quiz Title: 
-        <input className="question-input" value={this.state.title} onChange={this.setTitle} type="text" name="title" />
-        {this.validator.message('title', this.state.title, 'required|min:3|max:120')}
-      </label></div>
-    <br /><br />
-    <hr /><br />
-    <div>
-      {this.state.questionList}
-    </div>
-        <button type="button" className='main-button alternate' onClick={this.onAddBtnClick}>Add Question</button><br />
-        <button type="button" className='main-button' onClick={this.submitForm}>Create!</button>
-    </form>
-    </div>
-  </div>
-  );
-}
+      <div id='createQuiz'>
+        <div className='game-header alternate'>
+          <h2>The InQUIZition</h2>
+        </div>
+        <div className='game-body'>
+          <form className='form-body'>
+            <h2>Build Your Quiz</h2>
+            <div className='form-group'>
+              <label>
+                Username:
+                <input
+                  className='question-input'
+                  onChange={this.setUsername}
+                  value={this.state.username}
+                  type='text'
+                  name='username'
+                />
+                {this.validator.message(
+                  "username",
+                  this.state.username,
+                  "required|min:3|max:40"
+                )}
+              </label>
+            </div>
+            <br />
+            <div className='form-group'>
+              <label>
+                Quiz Title:
+                <input
+                  className='question-input'
+                  value={this.state.title}
+                  onChange={this.setTitle}
+                  type='text'
+                  name='title'
+                />
+                {this.validator.message(
+                  "title",
+                  this.state.title,
+                  "required|min:3|max:120"
+                )}
+              </label>
+            </div>
+            <br />
+            <br />
+            <hr />
+            <br />
+            <div>{this.state.questionList}</div>
+            <button
+              type='button'
+              className='main-button alternate'
+              onClick={this.onAddBtnClick}>
+              Add Question
+            </button>
+            <br />
+            <button
+              type='button'
+              className='main-button'
+              onClick={this.submitForm}>
+              Create!
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default CreateQuiz;
+export default withRouter(CreateQuiz);
--- client/src/Components/Quiz/Quiz.js
+++ client/src/Components/Quiz/Quiz.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 function Quiz() {
-  return <h1>Playable Quiz Goes here</h1>;
+  return (
+    <div id='quiz'>
+      <div className='game-header alternate'>
+        <Link to='/'>
+          <h2>The InQUIZition</h2>
+        </Link>
+      </div>
+      <div className='game-body'>
+        <div className='form-body'>
+          <h2>title</h2>
+          <label>
+            Share this link with you friends:
+            <input
+              className='question-input'
+              type='text'
+              name='share-link'
+              readOnly
+            />
+          </label>
+          <br />
+          <button type='button' className='main-button'>
+            Start!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Quiz;
+
+export default withRouter(Quiz);
